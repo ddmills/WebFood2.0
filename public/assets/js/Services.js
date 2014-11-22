33657cd223e3e45f196beba8705be6a1677eb6b2
dmills <dmills@iastate.edu>
2014-11-22 16:22:31 -0600

    Showing different eating Locations on the homepage now

--- public/assets/js/Services.js
+++ public/assets/js/Services.js
@@ -7,18 +7,25 @@ function Data() {
 
   /* array of locations - filled with initialize() function */
   self.locations = ko.observableArray();
-  self.items = {};
-  self.menus = {};
+  self.items     = ko.observableArray();
+  self.menus     = ko.observableArray();
 
+
+  self.locationRows = ko.computed(function() {
+    var locations = self.locations();
+    var result = [];
+    for (var i = 0; i < locations.length; i += 3) {
+      result.push(locations.slice(i, i+3));
+    }
+    console.log(result);
+    return result;
+  });
 
   /* parse the locations xml - returns a promise */
   ParseData.getLocations().then(function(data) {
-    self.locations = ko.observableArray(data);
-    // for (var i = 0; i < data.length; i++) {
-    //   self.locations.push(data[i]);
-    //   console.log('pushed');
-    // }
+    data.forEach(function(val) {
+      self.locations.push(val);
+    });
     console.log(data);
-    // console.log(self.locations);
   });
 }
